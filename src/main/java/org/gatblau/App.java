aa8fd8cf4b4c16b78fa0bfe057ec4adb079072d1
gatblau <andres.alos.m@gmail.com>
2017-11-19 12:24:08 +0000

    Adds NO_PROXY variable configuration.

--- src/main/java/org/gatblau/App.java
+++ src/main/java/org/gatblau/App.java
@@ -15,14 +15,24 @@ import java.util.Map;
 @SpringBootApplication
 public class App {
 
-    @Value("${DESTINATION_URL}")
+    @Value("${DESTINATION_URL:_}")
     private String _destinationURL;
 
     @Value("${URL_MAPPINGS:/*}")
     private String _urlMappings;
 
+    @Value("${NO_PROXY:_}")
+    private String _no_proxy;
+
     @Bean
     public ServletRegistrationBean proxyServlet(final ServletConfig dispatcherServlet) throws ServletException {
+        if (!_no_proxy.equals("_")) {
+            System.out.println(String.format("Setting NO_PROXY=%s", _no_proxy));
+            System.setProperty("NO_PROXY", _no_proxy);
+        }
+        if (_destinationURL.equals("_")) {
+            throw new RuntimeException("DESTINATION_URL has not been set.");
+        }
         ProxyServlet.Transparent proxyServlet = new ProxyServlet.Transparent();
         final Map<String, String> params = new HashMap<String, String>();
         params.put("proxyTo", _destinationURL);
